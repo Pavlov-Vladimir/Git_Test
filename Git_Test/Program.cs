fd559608ba00f2054a2969f209b43ad1ee30aaf4
Pavlov Vladimir <bob.pavlov.a7@gmail.com>
2021-07-15 13:20:03 +0300

    Add "Hi!"

--- Git_Test/Program.cs
+++ Git_Test/Program.cs
@@ -1,9 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Git_Test
 {
@@ -11,6 +6,7 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi!");
         }
     }
 }
